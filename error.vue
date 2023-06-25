7e649128003f7e1eaad62fb36b2d88de04e1ac0c
learntheropes <giovanni@learntheropes.xyz>
2023-06-25 11:35:59 +0200

    improve error page

--- error.vue
+++ error.vue
@@ -1,24 +1,3 @@
-<template>
-  <div class="hero is-fullheight-with-navbar">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <p v-if="error.statusCode === 404" class="title">{{ $t('pageNotFound') }}</p>
-        <p v-else-if="error.statusCode === 401" class="title">{{ $t('unauthorized') }}</p>
-        <p v-else class="title">{{$t('somethingWentWrong')}}</p>
-        <DevOnly class="block">
-          <div class="content">
-            {{ error.message }}<br>
-            {{ error.stack }}
-          </div>
-        </DevOnly>
-        <NuxtLink :to="localePath({ name: 'index' })">
-          <button class="button is-primary is-outlined">{{ $t('backToTheHomePage') }}</button>
-        </NuxtLink>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script setup>
 const props = defineProps({
   error: Object,
@@ -29,3 +8,24 @@ useContentHead({
   description: props.error.message
 })
 </script>
+
+<template>
+  <div class="hero is-fullheight-with-navbar">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <p v-if="error.statusCode === 404" class="title">{{ $t('pageNotFound') }}</p>
+        <p v-else-if="error.statusCode === 401" class="title">{{ $t('unauthorized') }}</p>
+        <p v-else class="title">{{$t('somethingWentWrong')}}</p>
+        <DevOnly class="block">
+          <div class="content">
+            <div>{{ error.message }}</div>
+            <div>{{ error.stack }}</div>
+          </div>
+        </DevOnly>
+        <NuxtLink :to="localePath({ name: 'index' })">
+          <button class="button is-primary is-outlined">{{ $t('backToTheHomePage') }}</button>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
