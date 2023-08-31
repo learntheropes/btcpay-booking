21a27bbba3975cb724887565b415052d5c6ca9ff
learntheropes <giovanni@learntheropes.xyz>
2023-08-31 11:27:52 +0200

    remove unused vee-validate locales

--- plugins/validateRules.js
+++ plugins/validateRules.js
@@ -1,47 +1,20 @@
 import { defineRule, configure } from "vee-validate";
 import { localize  } from '@vee-validate/i18n';
 import { required, email } from '@vee-validate/rules';
-import { locales } from '../assets/js/locales';
+
+import en from '@vee-validate/i18n/dist/locale/en.json';
+import es from '@vee-validate/i18n/dist/locale/es.json';
+import it from '@vee-validate/i18n/dist/locale/it.json'
 
 export default defineNuxtPlugin(nuxtApp => {
 
-  const localizeMessages = locales.reduce((obj, locale) => {
-
-    switch(locale.code) {
-      case 'ms':
-        locale.code = 'ms_MY'
-        break;
-      case 'nb':
-        locale.code = 'nb_NO'
-        break;
-      case 'nb':
-        locale.code = 'nb_NO'
-        break;
-      case 'br':
-        locale.code = 'pt_BR'
-        break;
-      case 'pt':
-        locale.code = 'pt_PT'
-        break;
-      case 'sr':
-        locale.code = 'sr_Latin'
-        break;
-      case 'cn':
-        locale.code = 'zh_CN'
-        break;
-      case 'tw':
-        locale.code = 'zh_TW'
-        break;
-    }
-    const module = import(`../node_modules/@vee-validate/i18n/dist/locale/${locale.code}.json`)
-
-    obj[locale.code] = module;
-    return obj;
-  }, {});
-
   // configure localized messages
   configure({
-    generateMessage: localize(localizeMessages),
+    generateMessage: localize({
+      en,
+      es,
+      it
+    }),
   });
 
   // Define default rules
